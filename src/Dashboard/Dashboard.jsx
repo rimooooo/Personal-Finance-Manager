7dfa6862086deccb47c2d75598cb6fd2b4c70ba3
RimjhimBansal <rimjhimbansal0@gmail.com>
2025-02-23 14:03:25 +0530

    first commit

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,22 @@
+import "./Dashboard.css"
+
+export default function Dashboard() {
+   return (
+      <div className="main">
+         <div className="home-icon">
+            <i class="fa-solid fa-swatchbook"></i> 
+         </div>
+         <div className="icons">
+            <i class="fa-solid fa-square-pen"></i>
+            <i class="fa-solid fa-wallet"></i>
+            <i class="fa-solid fa-chart-simple"></i>
+            <i class="fa-sharp fa-solid fa-cube"></i>
+            <i class="fa-regular fa-circle-user"></i>
+         </div>
+         <div className="icons1">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+         </div>
+         
+      </div>
+   )
+}

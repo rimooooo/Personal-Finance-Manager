6ac808726d0841880ab4488a1c3503ecace9e96d
RimjhimBansal <rimjhimbansal0@gmail.com>
2025-02-23 21:53:50 +0530

    Dashboard UI

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ export default function Dashboard() {
    return (
       <>
       <div className="main">
+         <div className="navbar">
          <div className="home-icon">
             <i class="fa-solid fa-swatchbook"></i> 
          </div>
@@ -16,6 +17,55 @@ export default function Dashboard() {
          </div>
          <div className="icons1">
             <i class="fa-solid fa-arrow-right-from-bracket"></i>
+         </div>
+         </div>
+         <div className="main-content">
+
+            <div className="box">
+               <div className="box-info">Overview</div>
+               <div className="box-icons">
+               <i class="fa-solid fa-magnifying-glass"></i>
+               <i class="fa-regular fa-bell"></i>
+               <i class="fa-solid fa-user">User</i>
+               </div>
+            </div>
+            <div className="box-1">
+               <div className="box12">
+                  Portfolio
+                  <div className="innerbox">
+                     Balance
+                     $1,23,000
+                  </div>
+               </div>
+               <div className="box1">
+                  <div style={{paddingLeft:"20px"}}>
+                  Category
+                  </div>
+                  <div className="box13">
+                  <div className="box11">
+                     1
+                  </div>
+                  <div className="box11">
+                     2
+                  </div>
+                  <div className="box11">
+                     3
+                  </div>
+                  </div>
+               </div>
+            </div>
+            <div className="box-2">
+               <div className="box2">
+                  <div className="box21">
+
+                  </div>
+               </div>
+               <div className="box2">
+                  <div className="box21">
+
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
       
